87e9b2ba492362914799efd2f51efe9a4680235d
juliejeong <julie.seoyeon.jeong@gmail.edu>
2023-04-18 17:35:13 -0400

    added release code

---
+++ release/data/download.sh
@@ -0,0 +1,24 @@
+curl -O http://vision.seas.harvard.edu/qsfs/PSData.zip
+#curl -O https://vision.middlebury.edu/stereo/data/scenes2014/zip/Adirondack-perfect.zip
+#curl -O https://vision.middlebury.edu/stereo/data/scenes2014/zip/Backpack-perfect.zip
+#curl -O https://vision.middlebury.edu/stereo/data/scenes2014/zip/Bicycle1-perfect.zip
+#curl -O https://vision.middlebury.edu/stereo/data/scenes2014/zip/Cable-perfect.zip
+#curl -O https://vision.middlebury.edu/stereo/data/scenes2014/zip/Classroom1-perfect.zip
+#curl -O https://vision.middlebury.edu/stereo/data/scenes2014/zip/Couch-perfect.zip
+curl -O https://vision.middlebury.edu/stereo/data/scenes2014/zip/Flowers-perfect.zip
+#curl -O https://vision.middlebury.edu/stereo/data/scenes2014/zip/Jadeplant-perfect.zip
+#curl -O https://vision.middlebury.edu/stereo/data/scenes2014/zip/Mask-perfect.zip
+#curl -O https://vision.middlebury.edu/stereo/data/scenes2014/zip/Motorcycle-perfect.zip
+#curl -O https://vision.middlebury.edu/stereo/data/scenes2014/zip/Piano-perfect.zip
+#curl -O https://vision.middlebury.edu/stereo/data/scenes2014/zip/Playroom-perfect.zip
+#curl -O https://vision.middlebury.edu/stereo/data/scenes2014/zip/Recycle-perfect.zip
+#curl -O https://vision.middlebury.edu/stereo/data/scenes2014/zip/Shelves-perfect.zip
+#curl -O https://vision.middlebury.edu/stereo/data/scenes2014/zip/Shopvac-perfect.zip
+#curl -O https://vision.middlebury.edu/stereo/data/scenes2014/zip/Sticks-perfect.zip
+#curl -O https://vision.middlebury.edu/stereo/data/scenes2014/zip/Storage-perfect.zip
+#curl -O https://vision.middlebury.edu/stereo/data/scenes2014/zip/Sword1-perfect.zip
+#curl -O https://vision.middlebury.edu/stereo/data/scenes2014/zip/Sword2-perfect.zip
+#curl -O https://vision.middlebury.edu/stereo/data/scenes2014/zip/Umbrella-perfect.zip
+#curl -O https://vision.middlebury.edu/stereo/data/scenes2014/zip/Vintage-perfect.zip
+
+find . -name "*.zip" -exec unzip {} \;
